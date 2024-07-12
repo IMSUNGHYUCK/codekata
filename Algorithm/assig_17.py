99088ad0a20cbc99a5cd90fb9b0b5995548708d4
IMSUNGHYUCK <dlatjdrnf@naver.com>
2024-07-12 15:50:41 +0900

    완료

--- Algorithm/assig_17.py
+++ Algorithm/assig_17.py
@@ -1,28 +1,7 @@
-import random
+def solution(n):
+    answer = []
+    reversed_str = str(n)[::-1]
 
-
-def up_down_game():
-    number_to_guess = random.randint(1, 1000000)
-    attempts = 0
-    low, high = 1, 1000000
-
-    print("컴퓨터가 1부터 100 사이의 숫자를 맞춰봅니다.")
-
-    while True:
-        guess = (low + high) // 2
-        attempts += 1
-        print(f"컴퓨터의 추측: {guess}")
-
-        if guess < number_to_guess:
-            print("업!")
-            low = guess + 1
-        elif guess > number_to_guess:
-            print("다운!")
-            high = guess - 1
-        else:
-            print(f"맞췄습니다! 숫자는 {guess}였습니다. 시도 횟수: {attempts}")
-            break
-
-
-if __name__ == "__main__":
-    up_down_game()
+    for char in reversed_str:
+        answer.append(int(char))
+    return answer
