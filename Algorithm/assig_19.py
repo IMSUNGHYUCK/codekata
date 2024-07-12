a87454c3416759b0b0e38b60de9a37f6fff9313d
IMSUNGHYUCK <dlatjdrnf@naver.com>
2024-07-12 16:25:49 +0900

    완료

--- Algorithm/assig_19.py
+++ Algorithm/assig_19.py
@@ -8,3 +8,15 @@ def solution(n):
         return -1
 
 print(solution(4))
+
+
+num = range(1,90)
+b = []
+for i in num:
+    p = i ** 0.5
+    if p == int(p):
+        b.append((p+1)**2)
+    else:
+        b.append(-1)
+
+print(b)
