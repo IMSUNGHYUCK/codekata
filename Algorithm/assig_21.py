e422ffde06c32dd7b833fb6929332dd34e560bb6
IMSUNGHYUCK <dlatjdrnf@naver.com>
2024-07-12 17:25:45 +0900

    완료

---
+++ Algorithm/assig_21.py
@@ -0,0 +1,15 @@
+def solution(x):
+    y = []
+    for i in str(x):
+        y.append(int(i))
+
+    p = x / sum(y)
+
+    if p == int(p):
+        answer = '하샤드 수 입니다.'
+    else:
+        answer = '하샤드 수가 아닙니다.'
+
+    return answer
+
+print(solution(9))
