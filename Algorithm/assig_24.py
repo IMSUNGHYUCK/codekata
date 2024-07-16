d39280227e6773ceca433461e5ee207008f638a1
IMSUNGHYUCK <dlatjdrnf@naver.com>
2024-07-16 20:10:41 +0900

    완료

--- Algorithm/assig_24.py
+++ Algorithm/assig_24.py
@@ -1,4 +1,7 @@
 def solution(seoul):
-    for i, v in enumerate(seoul):
+    for i,v in enumerate(seoul):
         if v == "Kim":
-            return f'김서방은 {i}에 있다'
+            answer = print(f'"김서방은 {i}에 있다"')
+    return answer
+
+print(solution(["Jane", "Kim"]))
