e0869fac824f8bca1d75ac079f6d7515d1cd3c6e
IMSUNGHYUCK <dlatjdrnf@naver.com>
2024-07-22 09:42:17 +0900

    완료

--- Algorithm/assig_21.py
+++ Algorithm/assig_21.py
@@ -1,14 +1,14 @@
-def solution(x):
-    y = []
-    for i in str(x):
-        y.append(int(i))
+def solution(x): #값 x을 불러올 solution 함수 생성
+    y = [] # 리스트 생성
+    for i in str(x): #i 안에다 x를 문자형으로 바꿔서 인덱스값 만큼 반복
+        y.append(int(i)) #리스트 y에다 숫자형으로 바꾼 i 저장
 
-    p = x / sum(y)
+    p = x / sum(y) # p변수에다 x에 리스트 y값을 합하여 저장
 
-    if p == int(p):
-        answer = '하샤드 수 입니다.'
+    if p == int(p): #p와 정수형 p가 같으면
+        answer = True # True 반환
     else:
-        answer = '하샤드 수가 아닙니다.'
+        answer = False # 아니면 False 반환
 
     return answer
 
---
+++ Algorithm/assig_38.py
@@ -0,0 +1,5 @@
+n, m = map(int, input().split())
+
+# 별 문자를 이용해 직사각형 출력
+for _ in range(m):
+    print('*' * n)
